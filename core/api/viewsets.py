5f285f49ec5047d74f4a88adbe0155ba10d2cf6e
whoisjose569 <jose.vitor.araujo569@hotmail.com>
2023-07-20 16:45:58 -0300

    alterando o lookup_field padrão do endpoint

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -10,6 +10,8 @@ class PontoTuristicoViewSet(ModelViewSet):
     serializer_class = PontoTuristicoSerializer
     filter_backends = [SearchFilter]
     search_fields = ['nome', 'descricao', 'endereco__linha1']
+    #O lookup_field precisa ser unico 
+    #lookup_field = 'nome'
     
     def get_queryset(self):
         id = self.request.query_params.get('id', None)
@@ -29,30 +31,30 @@ class PontoTuristicoViewSet(ModelViewSet):
         #return PontoTuristico.objects.filter(aprovado=True)
         return queryset
     
-    def list(self, request, *args, **kwargs):
-        return super(PontoTuristicoViewSet, self).list(request, *args, **kwargs)
+    # def list(self, request, *args, **kwargs):
+    #     return super(PontoTuristicoViewSet, self).list(request, *args, **kwargs)
     
-    def create(self, request, *args, **kwargs):
-        return super(PontoTuristicoViewSet, self).create(request, *args, **kwargs)
+    # def create(self, request, *args, **kwargs):
+    #     return super(PontoTuristicoViewSet, self).create(request, *args, **kwargs)
     
-    def destroy(self, request, *args, **kwargs):
-        return super(PontoTuristicoViewSet).destroy(request, *args, **kwargs)
+    # def destroy(self, request, *args, **kwargs):
+    #     return super(PontoTuristicoViewSet).destroy(request, *args, **kwargs)
     
-    def retrieve(self, request, *args, **kwargs):
-        return super(PontoTuristicoViewSet).retrieve(request, *args, **kwargs)
+    # def retrieve(self, request, *args, **kwargs):
+    #     return super(PontoTuristicoViewSet).retrieve(request, *args, **kwargs)
     
-    def update(self, request, *args, **kwargs):
-        return super(PontoTuristicoViewSet).update(request, *args, **kwargs)
+    # def update(self, request, *args, **kwargs):
+    #     return super(PontoTuristicoViewSet).update(request, *args, **kwargs)
     
-    def partial_update(self, request, *args, **kwargs):
-        return super(PontoTuristicoViewSet).partial_update(request, *args, **kwargs)
+    # def partial_update(self, request, *args, **kwargs):
+    #     return super(PontoTuristicoViewSet).partial_update(request, *args, **kwargs)
     
-    #Criar novas açoes
-    @action(methods=['get'], detail=True)
-    def denunciar(self, request, pk=None):
-        pass
+    # Criar novas açoes
+    # @action(methods=['get'], detail=True)
+    # def denunciar(self, request, pk=None):
+    #     pass
     
-    @action(methods=['get'], detail=False)
-    def teste(self, request):
-        pass
+    # @action(methods=['get'], detail=False)
+    # def teste(self, request):
+    #     pass
         
