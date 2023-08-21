11cd364eb13f5c06bb7f9b02be611654ce917097
whoisjose569 <jose.vitor.araujo569@hotmail.com>
2023-08-21 14:14:00 -0300

    Criando Actions Personalizadas

--- comentarios/admin.py
+++ comentarios/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Comentario
+from .actions import reprova_comentarios, aprova_comentarios
+
 
 @admin.register(Comentario)
 class ComentarioAdmin(admin.ModelAdmin):
     list_display = ['usuario', 'comentario', 'data', 'aprovado']
+    actions = [reprova_comentarios, aprova_comentarios]
--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -135,3 +135,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
+}
